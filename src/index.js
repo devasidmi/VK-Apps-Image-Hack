522ef525f0bf91034d0bde37fdd69ca8b76e8e4b
Vasiliy Dmitriev <vasidmi@yandex-team.ru>
2020-09-14 22:14:25 +0300

    fixed unsecure requests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import 'core-js/es6/map';
 import 'core-js/es6/set';
+
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import bridge from '@vkontakte/vk-bridge';
-import App from './App';
 
 bridge.send('VKWebAppInit', {});
 
 ReactDOM.render(<App />, document.getElementById('root'));
+import ("./eruda").then(({default: eruda}) => {});
